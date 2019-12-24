4a17f0b0d9dbc73c483e75ac77473090dc996c74
Calvin Lowe <calvin.lowe@roberthalf.com>
2019-12-23 16:30:12 -0800

    updated container files

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import Header from './items.js';
-
+import Paper from '@material-ui/core/Paper';
+// import Typography from '@material-ui/core/Typography';
+import {PicTextLayout} from './items.js';
 //Home Section
+
 export default class HomeContainer extends React.Component{
 
   render(){
     return(
       <div>
         <Header className="mb-3" title={"About Me"}/>
-        <Container className="containerStyle" maxWidth="lg">
-          
-          <p class="text-center"> hello </p>
-
+        <Container maxWidth="lg">
+            <PicTextLayout text="test" />
         </Container>
       </div>
     );
--- src/Components/items.js
+++ src/Components/items.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Paper from '@material-ui/core/Paper';
 
 export default class Header extends React.Component{
 
   render(){
     return(
       <h1 className="text-center my-4 mx-1 mx-md-0 headerStyle fontStyling"> {this.props.title} </h1>
+    );
+  }
+}
+
+export class PicTextLayout extends React.Component{
+  render(){
+    return(
+      <div>
+        <Grid container>
+          <Grid className="m-2 whiteBack" item xs={2}>
+            <p className="m-2">{this.props.text}</p>
+          </Grid>
+          <Grid className="m-2 whiteBack"item xs={9}>
+            <p className="m-2">{this.props.text}</p>
+          </Grid>
+        </Grid>
+      </div>
     );
   }
 }
