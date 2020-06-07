a11a2de9fdfb7e53ee5e0b584479dc0a2c612ed2
Cwlowe <calvinwolfelowe@gmail.com>
2020-06-07 14:12:03 -0700

    UA - Added CNAME and logos

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -4,12 +4,14 @@ import {Link} from 'react-router-dom';
 //Navbar section
 export default function Navbar({updateContent}){
     return(
-      <ul className="navBarStyle ">
+        <ul className="navBarStyle ">
+          <img alt="Calvin wolfe lowe. Enjoy life to the fullest" src="src/img/Callogo.png" />
           <Navitems id="0" text="Home" url="/"/>
           <Navitems id="1" text="About Me" url="/aboutme" />
           <Navitems id="2" text="Photography" url="/photography" />
           <Navitems id="3" text="Blog" url="/blog" />
       </ul>
+      
     );
 }
 // Navitems
