76fb9f2338a3da7b11a5adfcd9398ffcc57dd12e
Cwlowe <calvinwolfelowe@gmail.com>
2020-09-26 01:50:08 -0700

    changed 404 page

--- src/Components/Pages/404.js
+++ src/Components/Pages/404.js
@@ -6,13 +6,20 @@ import Footer from '../Footer';
 const errorPage = ()=>{
     const style = {
         height:"33vh",
+        margin:"5%",
     }
     return (
     <React.Fragment>
         <BannerContainer />
         <div style= {style}>
-            <h1 className="h2Styling text-center m-4">404</h1>
-            <h2 className="h2Styling text-center m-4">This page is not avaiable</h2>
+            <div className="errorBox">
+                <div className="h1Styling text-center m-4">
+                    <i class="fas fa-exclamation-triangle"></i> 
+                </div>
+                <h1 className="h1Styling text-center m-4">404</h1>
+                <h2 className="h2Styling text-center m-4">This page is not avaiable</h2>
+            </div>
+            
         </div>
         <Footer/>
     </React.Fragment>
